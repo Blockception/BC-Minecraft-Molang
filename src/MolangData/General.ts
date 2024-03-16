a22018b4bec4a2ccece79e1d084bd076c52c3ccd
Daan Verstraten <daanverstraten@hotmail.com>
2024-03-16 09:54:53 +0100

    Update src/MolangData/General.ts

--- src/MolangData/General.ts
+++ src/MolangData/General.ts
@@ -729,7 +729,7 @@ export namespace General {
     {
       id: "is_attached",
       documentation: "Returns 1.0 if the entity is attached to another entity (such as being held or worn), else it will return 0.0. Available only with resource packs." 
-    }
+    },
     {
       id: "is_attached_to_entity",
       documentation: "Returns 1.0 if the actor is attached to an entity, else it will return 0.0.",
