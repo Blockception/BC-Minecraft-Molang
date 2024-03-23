2680ce5424c67c882881e59d04f4c01d25f42abc
DaanV2 <daanverstraten@hotmail.com>
2024-03-23 08:10:11 +0100

    Adding variables

--- src/MolangData/Attachables.ts
+++ src/MolangData/Attachables.ts
@@ -3,7 +3,7 @@ import { Data } from "./Data";
 /**Molang data for animations controllers*/
 export namespace Attachables {
   /**The list of usable variables for this specific type*/
-  export const Variables: Data[] = [];
+  export const Variables: Data[] = [{id: "is_enchanted"}];
   /**The list of specific contexts usable for this specific type*/
   export const Contexts: Data[] = [{ id: "is_first_person" }, { id: "item_slot" }];
   /**The list of usable temp variables for this specific type*/
